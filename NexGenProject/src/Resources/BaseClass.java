0bf17701d3f49a84eaf87cbb16a336791f91aad0
pc <pc@DESKTOP-DKHR9Q5>
2021-01-20 19:06:50 +0530

    commit and push

---
+++ NexGenProject/src/Resources/BaseClass.java
@@ -0,0 +1,15 @@
+package Resources;
+
+public class BaseClass {
+
+	public static void main(String[] args)
+	
+	{
+		System.setProperty("WebDriver.driver.chrome", "C:\\Users\\pc\\Downloads\\Driver\\New\\chromedriver.exe");
+		
+		Webdriver driver= new chromeDriver();
+		
+
+	}
+
+}
